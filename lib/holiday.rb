5f536315528adc2528d9d3eb583ce24b447937b0
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-15 20:01:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,8 +73,9 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  holiday_hash.each do |season, event|
+    event.each do |key, value|
+      
 
 end
 
